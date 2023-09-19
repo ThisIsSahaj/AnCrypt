695287ba4b5cdb0efeada432e15cc63d04b11b77
Sahaj Jain <s21sahaj@gmail.com>
2023-09-19 22:38:45 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -51,10 +51,6 @@ const Header = () => {
 
            {user && <Link className={classes.portfolio} to={`/portfolio/${user.displayName || user.email}`}>Watchlist</Link>  } 
 
-
-           {user && <Link className={classes.portfolio} to={`/portfolio/${user.displayName || user.email}`}>Portfolio</Link>  } 
-
-
            <Select variant="outlined" style={{width:100, height:40, marginRight:15, zIndex:"1",}}
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
